6c838f3154bb2efef30e32cd3b4be01c33c75795
Krugou <59641229+Krugou@users.noreply.github.com>
2022-05-01 15:44:46 +0300

    video lisäys pääikkunaan

--- app/src/main/java/com/example/terveyssovellusryhm8/MainActivity.java
+++ app/src/main/java/com/example/terveyssovellusryhm8/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.terveyssovellusryhm8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.VideoView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VideoView videoView = (VideoView) findViewById(R.id.videoView2);
+        Random sekalainen = new Random();
+        int videoNumero = sekalainen.nextInt((6 - 1) + 1) + 1;
+        if (videoNumero == 1){
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.mental1);
+    } else if ( videoNumero == 2){
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.mental2);
+    } else if ( videoNumero == 3){
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.mental3);
+    } else if ( videoNumero == 4){
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.mental4);
+    } else if ( videoNumero == 5){
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.mental5);
+
+    } else if ( videoNumero == 6){
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.mental6);
+
+    }videoView.start();
+        videoView.setOnCompletionListener ( new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                videoView.start();
+            }
+        });
     }
     //Metodi, joka siirtää päiväkirja-aktiviteettiin napin painalluksella.
     public void openDiary(View v){
