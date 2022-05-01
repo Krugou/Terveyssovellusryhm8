4c023e63feffc524f7e8d36a2196d88c9ccdac0c
Krugou <59641229+Krugou@users.noreply.github.com>
2022-05-02 01:06:06 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/terveyssovellusryhm8/MainActivity.java
+++ app/src/main/java/com/example/terveyssovellusryhm8/MainActivity.java
@@ -12,43 +12,8 @@ import android.widget.VideoView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        VideoView videoView = (VideoView) findViewById(R.id.videoView2);
-        Random sekalainen = new Random();
-        int videoNumero = sekalainen.nextInt((6 - 1) + 1) + 1;
-        Log.i("my_app", String.valueOf(videoNumero));
-        if (videoNumero == 1){
-            videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.mental1);
-        } else if ( videoNumero == 2){
-            videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.mental2);
-        } else if ( videoNumero == 3){
-            videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.mental3);
-        } else if ( videoNumero == 4){
-            videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.mental4);
-        } else if ( videoNumero == 5){
-            videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.mental5);
-
-        } else if ( videoNumero == 6){
-            videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.mental6);
-
-        }
-        videoView.start();
-        // looppi videolle
-        videoView.setOnCompletionListener ( new MediaPlayer.OnCompletionListener() {
-
-            @Override
-            public void onCompletion(MediaPlayer mediaPlayer) {
-                videoView.start();
-            }
-        });
-    }
-    @Override
-    protected void  onResume() {
-        super.onResume();
+    public void videoPlayLoop(){
+        // sekalainen video main aktiviteetille
         VideoView videoView = (VideoView) findViewById(R.id.videoView2);
         Random sekalainen = new Random();
         int videoNumero = sekalainen.nextInt((6 - 1) + 1) + 1;
@@ -77,6 +42,21 @@ public class MainActivity extends AppCompatActivity {
                 videoView.start();
             }
         });
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        videoPlayLoop();
+
+    }
+
+
+    @Override
+    protected void  onResume() {
+        super.onResume();
+        videoPlayLoop();
+
     }
     //Metodi, joka siirtää päiväkirja-aktiviteettiin napin painalluksella.
     public void openDiary(View v){
