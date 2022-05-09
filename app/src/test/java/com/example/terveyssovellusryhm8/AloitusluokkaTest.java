1475cfeead7cf34a53d72eff6eac81d09fc2cc15
Krugou <59641229+Krugou@users.noreply.github.com>
2022-05-09 13:57:32 +0300

    Update AloitusluokkaTest.java

--- app/src/test/java/com/example/terveyssovellusryhm8/AloitusluokkaTest.java
+++ app/src/test/java/com/example/terveyssovellusryhm8/AloitusluokkaTest.java
@@ -13,25 +13,35 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/*
+* Test class for the class Aloitusluokka
+* @author Aleksi
+*/
 public class AloitusluokkaTest {
     private List<Positiivisetsanat> positivisetSanatLista;
     private final String testiString = "test";
     private final String testiString2 = "test2";
 
+    /*
+    * Sets up the test environment
+    */
 
     @Before
     public void setUp() {
         positivisetSanatLista = new ArrayList<>();
 
-
-
     }
+    /*
+    * Tests the constructor of the class Aloitusluokka
+    */
     @Test
-    public void testAloitusluokkaInit(){
+    public void testAloitusluokkaInit() {
         assertTrue(positivisetSanatLista.isEmpty());
         assertEquals(0, positivisetSanatLista.size());
     }
+    /*
+    * Tests the addPositivisetSanat method of the class Aloitusluokka
+    */
     @Test
     public void testAloitusluokkaAdd() {
         positivisetSanatLista.add(new Positiivisetsanat(testiString, testiString2));
@@ -39,11 +49,12 @@ public class AloitusluokkaTest {
         assertEquals(testiString, positivisetSanatLista.get(0).getPositiivisetSanat());
         assertEquals(testiString2, positivisetSanatLista.get(0).getPositiivisetSanatTekija());
 
-
-
     }
+    /*
+    * Tests the getPositivisetSanat method of the class Aloitusluokka
+    */
     @Test
-    public void testAloitusluokkaRemove(){
+    public void testAloitusluokkaRemove() {
         positivisetSanatLista.add(new Positiivisetsanat(testiString, testiString2));
         positivisetSanatLista.add(new Positiivisetsanat(testiString2, testiString));
         positivisetSanatLista.remove(0);
@@ -52,6 +63,9 @@ public class AloitusluokkaTest {
         assertEquals(testiString, positivisetSanatLista.get(0).getPositiivisetSanatTekija());
 
     }
+    /*
+    * Tests the getPositivisetSanat method of the class Aloitusluokka
+    */
     @Test
     public void testAloitusluokkaAreThere(){
         assertTrue(testiString==testiString);
